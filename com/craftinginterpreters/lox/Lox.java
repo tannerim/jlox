f2c93ff0985afb8ff6da1d590961e26fb6f06e68
tannerim <FN-21-87@outlook.com>
2023-03-19 16:11:12 -0600

    new code

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -9,14 +9,72 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  /**
+   * @param args
+   * @throws IOException
+   */
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-      System.exit(64); 
+      System.exit(64);
     } else if (args.length == 1) {
-      // runFile(args[0]);
+      runFile(args[0]);
     } else {
-      // runPrompt();
+      runPrompt();
     }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files(new String(bytes, Charset.defaultCharset()));
+
+    // Indicate an error in the exit code.
+    if (hadError)
+      System.exit(65);
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) { 
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+    }
+  }
+  private static void run(String source){
+    static void error(int line, String message) {
+      report(line, "", message);
+    }
+  
+    private static void report(int line, String where,
+                               String message) {
+      System.err.println(
+          "[line " + line + "] Error" + where + ": " + message);
+      hadError = true;
+    }
+  }
+  private void number() {
+    while (isDigit(peek()))
+      advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek()))
+        advance();
+    }
+
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length())
+      return '\0';
+    return source.charAt(current + 1);
   }
 }
---
+++ com/craftinginterpreters/lox/lox/jlox/Token.java
@@ -0,0 +1,5 @@
+package com.craftinginterpreters.lox.lox.jlox;
+
+public class Token {
+    
+}
